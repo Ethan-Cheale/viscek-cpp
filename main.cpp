3d671c5eaec9d99643a972d826b1e815d10d9c1d
Ethan Cheale <ethancheale@gmail.com>
2025-01-20 15:00:04 +0000

    Added base framework for model

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include "system.h"
+
+int main(){
+    System test;
+    return 0;
+}
---
+++ system.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "system.h"
+
+// The update rule
+void System::updateRule(){
+    std::cout<<"Updating the system...S";
+}
+
+// Defining the constructor
+System::System(){
+    std::cout<<"I am constructing the System!\n";
+}
