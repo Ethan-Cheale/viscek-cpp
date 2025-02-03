5c7492f31a72ad1c883b989a745db72aefe34c52
Ethan Cheale <ethancheale@gmail.com>
2025-02-03 12:43:20 +0000

    Updated cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,14 @@
 #include "system.h"
 
 int main(){
+    std::string root = "frames/frame";
     System model(100, 20.0, 0.5, 0.1, 2025);
     model.randomStart();
-    std::string root = "frames/frame";
+    model.saveConfig(root + std::to_string(0));
 
-    for(int i = 0; i<= 5; i++){
-        model.saveConfig(root + std::to_string(i));
+    for(int i = 1; i<= 5; i++){
         model.updateRule();
+        model.saveConfig(root + std::to_string(i));
     }
 
     return 0;
